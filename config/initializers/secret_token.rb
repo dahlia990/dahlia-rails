e73a5d53e253415597275fd1cfbdd81163f2c79d
dahlia <dahlia.kamaran@gmail.com>
2013-03-24 16:49:21 +0300

    initiall commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DahliaRails::Application.config.secret_token = 'c11061ebe4988eafad1248e5430c436458055d518871bebd408786e069f1a225fb9ffd613a49ad1f2ac8b505679d856b1093df7b32d4c831e4e8c46b7c2b85ac'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+DahliaRails::Application.config.session_store :cookie_store, key: '_dahlia-rails_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# DahliaRails::Application.config.session_store :active_record_store
